fdb92f48f41264be05e6ce17832e416234bd4ff8
sveske_juice <carl.benjamin.dreyer@gmail.com>
2022-12-25 22:10:19 +0100

    better name

--- Assets/Scripts/Managers/EconomyManager/IBudgetInfluencer.cs
+++ Assets/Scripts/Managers/EconomyManager/IBudgetInfluencer.cs
@@ -1,17 +1,17 @@
 /**
 <summary>
-Every class that implements this interface will be a contributor
-to the player's budget. It will either contribute in a positive
+Every class that implements this interface will be an Influencer
+to the player's budget. It will either Influence in a positive
 (gives a profit) or negative way (costs more money sustain the class)
 </summary>
 **/
-public interface IBudgetContributor
+public interface IBudgetInfluencer
 {
     /// <summary>
     /// Interface method for how much the implementing class
-    /// contributes to the player's budget. Returns an amount
+    /// Influence to the player's budget. Returns an amount
     /// which will be summed from all other classes implementing
     /// this interface and give the player's total yearly budget/income.
     /// </summary>
-    double GetYearlyContribution();
+    double GetYearlyInfluence();
 }
